1688be2a832630d0c22e0aa965f7a39970d084e7
Seqoi <ddarikin123497@gmail.com>
2024-03-12 13:47:29 +0300

    Second commit of the first day

--- app/src/main/java/com/example/practice/SecondAcitivity.java
+++ app/src/main/java/com/example/practice/SecondAcitivity.java
@@ -2,6 +2,7 @@ package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SecondAcitivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class SecondAcitivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_acitivity);
+
+        Intent intent = new Intent(SecondAcitivity.this, ThirdActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
